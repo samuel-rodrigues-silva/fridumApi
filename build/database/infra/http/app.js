1d2e4da9d293044a59e6ae6d7538315ad48a4542
Samuel Silva <samuel-rodrigues-silva>
2021-07-27 16:54:01 -0300

    refactor: adding cors

--- build/database/infra/http/app.js
+++ build/database/infra/http/app.js
@@ -4,8 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
 var index_1 = __importDefault(require("./routes/index"));
 var app = express_1.default();
+var corsOptions = {
+    origin: '*',
+};
+app.use(cors_1.default(corsOptions));
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    next();
+});
 app.use(express_1.default.json());
 app.use(index_1.default);
 exports.default = app;
--- src/database/infra/http/app.ts
+++ src/database/infra/http/app.ts
@@ -9,12 +9,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-// app.use((request, response, next) => {
-//     response.header("Access-Control-Allow-Origin", "*");
-//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     response.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-//     next();
-// });
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    next();
+});
 
 app.use(express.json());
 app.use(routes);
