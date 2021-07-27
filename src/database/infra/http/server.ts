d4d79ad6b33cfc6b896144c77cad5ba522f99d2c
Samuel Silva <samuel-rodrigues-silva>
2021-07-27 11:34:30 -0300

    refactor: adding cors

--- src/database/infra/http/server.ts
+++ src/database/infra/http/server.ts
@@ -4,6 +4,12 @@ import 'reflect-metadata'
 import '../../index'
 
 
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("SERVER ON");
 });
