d4b48274597d0dcbd1f214d5e2e1c1701e9b3433
Samuel Silva <samuel-rodrigues-silva>
2021-07-27 12:43:13 -0300

    refactor: adding cors

--- src/database/infra/http/app.ts
+++ src/database/infra/http/app.ts
@@ -4,18 +4,17 @@ import routes from './routes/index';
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
+    origin: '*',
 }
 
 app.use(cors(corsOptions))
 
-app.use((request, response, next) => {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    next();
-});
+// app.use((request, response, next) => {
+//     response.header("Access-Control-Allow-Origin", "*");
+//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     response.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+//     next();
+// });
 
 app.use(express.json());
 app.use(routes);
