36fb175031b35595a25bfb9d8f4f160d0d79efe3
Samuel Silva <samuel-rodrigues-silva>
2021-08-31 09:00:33 -0300

    refactor: allow DELETE request CORS

--- src/database/infra/http/app.ts
+++ src/database/infra/http/app.ts
@@ -3,18 +3,7 @@ import cors from 'cors'
 import routes from './routes/index';
 const app = express();
 
-const corsOptions = {
-    origin: '*',
-}
-
-app.use(cors(corsOptions))
-
-app.use((request, response, next) => {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    next();
-});
+app.use(cors())
 
 app.use(express.json());
 app.use(routes);
