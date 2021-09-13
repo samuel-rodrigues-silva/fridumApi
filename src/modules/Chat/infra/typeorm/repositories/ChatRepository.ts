1155e26101129d1e3bf440dded19613f6e545897
Samuel Silva <samuel-rodrigues-silva>
2021-09-13 16:45:42 -0300

    feature: chat register Create

--- src/modules/Chat/infra/typeorm/repositories/ChatRepository.ts
+++ src/modules/Chat/infra/typeorm/repositories/ChatRepository.ts
@@ -20,7 +20,7 @@ class ChatRepository implements IChatRepository {
     }
 
     public async show(id: string): Promise<Chat[]> {
-        return await this.ormRepository.find({ where: { id: id }, relations: ['user', 'chatMessage'] });
+        return await this.ormRepository.find({ where: { id: id }, relations: ['user', 'chatMessage', 'chatMessage.user'] });
     }
 
     public async list(id: string): Promise<Chat[]> {
