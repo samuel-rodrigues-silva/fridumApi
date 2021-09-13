3fb3453ee62948408705391b3baf389e8199a925
Samuel Silva <samuel-rodrigues-silva>
2021-09-13 16:35:47 -0300

    feature: chat register Create

--- src/modules/Chat/infra/typeorm/entities/Chat.ts
+++ src/modules/Chat/infra/typeorm/entities/Chat.ts
@@ -15,7 +15,7 @@ export class Chat {
     @ManyToOne(() => User, (follow) => follow.id)
     follow: User
 
-    @OneToMany(() => ChatMessage, (chatMessage) => chatMessage.id, { cascade: true, onDelete: 'CASCADE' })
+    @OneToMany(() => ChatMessage, (chatMessage) => chatMessage.chat, { cascade: true, onDelete: 'CASCADE' })
     chatMessage?: ChatMessage[]
 
     @OneToOne(() => Service, (service) => service.id)
