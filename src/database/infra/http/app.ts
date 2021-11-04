8c38e606785274f962c139670824db4033215002
Samuel Silva <samuel-rodrigues-silva>
2021-11-04 10:25:03 -0300

    feat: applying multer image upload

--- src/database/infra/http/app.ts
+++ src/database/infra/http/app.ts
@@ -8,7 +8,8 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/image/:img', function (req, res) {
-    console.log(req);
+    const { img } = req.params
+    console.log(img);
 });
 
 app.use(routes);
