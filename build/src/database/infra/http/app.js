5fdd79464bb21d809666080c9c7da31973d6e1ef
Samuel Silva <samuel-rodrigues-silva>
2021-11-05 21:39:13 -0300

    feat: adding image get route

--- build/src/database/infra/http/app.js
+++ build/src/database/infra/http/app.js
@@ -6,13 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 var index_1 = __importDefault(require("./routes/index"));
+var path_1 = __importDefault(require("path"));
 var app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get('/image/:img', function (req, res) {
     var img = req.params.img;
-    console.log(img);
-    res.sendFile("./build/uploads/" + img);
+    console.log(path_1.default.resolve(__dirname, '../../../../', 'uploads'));
+    res.sendFile(path_1.default.resolve(__dirname, '../../../../', "uploads/" + img));
 });
 app.use(index_1.default);
 exports.default = app;
--- src/database/infra/http/app.ts
+++ src/database/infra/http/app.ts
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.get('/image/:img', function (req, res) {
     const { img } = req.params
-    console.log(img);
-    res.sendFile(`./build/uploads/${img}`);
+    console.log(path.resolve(__dirname, '../../../../', 'uploads'))
+    res.sendFile(path.resolve(__dirname, '../../../../', `uploads/${img}`));
 });
 
 app.use(routes);
