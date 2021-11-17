f22161eff7764856ec2a66f5701fdb2136ae57e3
Samuel Silva <samuel-rodrigues-silva>
2021-11-17 19:19:21 -0300

    refactor: cors shit

--- src/database/infra/http/app.ts
+++ src/database/infra/http/app.ts
@@ -4,15 +4,32 @@ import routes from './routes/index';
 import path from 'path'
 var app = express();
 
-app.use(cors({
-    origin: [
+app.use(cors())
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', [
         '*',
         'http://localhost:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:3001'
-    ]
-}))
+    ]);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(express.json());
 
