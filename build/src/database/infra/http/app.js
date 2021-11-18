48503e9c39c4cb32f34618b81f18b7aa6094a574
Samuel Silva <samuel-rodrigues-silva>
2021-11-18 18:26:40 -0300

    refactor: cors shit

--- build/src/database/infra/http/app.js
+++ build/src/database/infra/http/app.js
@@ -8,25 +8,13 @@ var cors_1 = __importDefault(require("cors"));
 var index_1 = __importDefault(require("./routes/index"));
 var path_1 = __importDefault(require("path"));
 var app = (0, express_1.default)();
-var originsWhitelist = [
-    'http://localhost:3000' //this is my front-end url for development
-];
-var corsOptions = {
-    origin: function (origin, callback) {
-        console.log(origin);
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        console.log(isWhitelisted);
-        callback(null, isWhitelisted);
-    },
-    credentials: true
-};
-app.use((0, cors_1.default)(corsOptions));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get('/image/:img', function (req, res) {
     var img = req.params.img;
     res.sendFile(path_1.default.resolve(__dirname, '../../../../', "uploads/" + img));
 });
-app.options('*', (0, cors_1.default)(corsOptions));
+app.options('*', (0, cors_1.default)());
 app.use(index_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/database/infra/http/app.ts
+++ src/database/infra/http/app.ts
@@ -4,21 +4,7 @@ import routes from './routes/index';
 import path from 'path'
 var app = express();
 
-var originsWhitelist = [
-    'http://localhost:3000'     //this is my front-end url for development
-
-];
-var corsOptions = {
-    origin: function (origin, callback) {
-        console.log(origin);
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        console.log(isWhitelisted);
-        callback(null, isWhitelisted);
-    },
-    credentials: true
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(express.json());
 
@@ -26,6 +12,6 @@ app.get('/image/:img', function (req, res) {
     const { img } = req.params
     res.sendFile(path.resolve(__dirname, '../../../../', `uploads/${img}`));
 });
-app.options('*', cors(corsOptions));
+app.options('*', cors());
 app.use(routes);
 export default app;
