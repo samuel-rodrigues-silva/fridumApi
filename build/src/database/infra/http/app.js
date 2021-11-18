b09d90cd4c4b89dec7bfb670d856eeba0f313c91
Samuel Silva <samuel-rodrigues-silva>
2021-11-18 11:11:20 -0300

    refactor: cors shit

--- build/src/database/infra/http/app.js
+++ build/src/database/infra/http/app.js
@@ -10,7 +10,7 @@ var path_1 = __importDefault(require("path"));
 var app = (0, express_1.default)();
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
