447752eec482f158b052211ad3858f8818c311f2
Alan Andrade <alanfrank@gec.inatel.br>
2021-11-19 08:40:24 -0300

    fix: fix ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,11 @@
 module.exports = {
    type: "mysql",
-   host: process.env.DB_HOST,
-   port: Number(process.env.DB_PORT),
-   username: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_SCHEMA_NAME,
+   url: process.env.JAWSDB_URL,
+   // host: process.env.DB_HOST,
+   // port: Number(process.env.DB_PORT),
+   // username: process.env.DB_USER,
+   // password: process.env.DB_PASSWORD,
+   // database: process.env.DB_SCHEMA_NAME,
    synchronize: false,
    entities: [
       process.env.NODE_ENV === 'development'
