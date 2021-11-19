fb13fc78aa61179b5291a4d242653d3565e02588
Alan Andrade <alanfrank@gec.inatel.br>
2021-11-19 08:33:53 -0300

    fix: fix vscode debugger

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,28 @@
-console.log('PROCESS:' + process.env.JAWSDB_URL);
 module.exports = {
-   "type": "mysql",
-   "url": process.env.JAWSDB_URL,
-   // "port": 3306,
-   // "username": "root",
-   // "password": "",
-   // "database": "fridum",
-   "synchronize": true,
-   "entities": [
-      "./build/src/modules/**/infra/typeorm/entities/*.js"
+   type: "mysql",
+   host: process.env.DB_HOST,
+   port: Number(process.env.DB_PORT),
+   username: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_SCHEMA_NAME,
+   synchronize: false,
+   entities: [
+      process.env.NODE_ENV === 'development'
+         ? './src/modules/**/infra/typeorm/entities/*.ts'
+         : './build/src/modules/**/infra/typeorm/entities/*.js',
+      process.env.NODE_ENV === 'development'
+         ? './src/modules/**/infra/typeorm/entities/*.ts'
+         : './dist/modules/**/infra/typeorm/entities/*.js',
    ],
-   "migrations": [
-      "./build/src/database/migration/*.js"
+   migrations: [
+      process.env.NODE_ENV === 'development'
+         ? './src/database/migration/*.ts'
+         : './build/src/database/migration/*.js',
    ],
-   "cli": {
-      "migrationsDir": "./src/database/migration",
-      "subscribersDir": "./src/subscriber",
-      "entitiesDir": "./src/modules/**/typeorm/entities"
+   cli: {
+      migrationsDir:
+         process.env.NODE_ENV === 'development'
+            ? './src/src/database/migration'
+            : '/dist/src/database/migration',
    }
 }
