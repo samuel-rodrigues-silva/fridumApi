4be2ae30513d711c74f2dfbdd8643657b32e3932
SamullSillva <samuka_55@hotmail.com>
2022-02-07 23:14:59 -0300

    refactor: adding service patch request body

--- src/modules/Service/infra/http/routes/Service.routes.ts
+++ src/modules/Service/infra/http/routes/Service.routes.ts
@@ -40,7 +40,7 @@ serviceRouter.patch('/:id', celebrate({
     },
     [Segments.BODY]: {
         status: Joi.string(),
-        unread: Joi.string().allow(null),
+        unread: Joi.bool().allow(null),
         finishedAt: Joi.date(),
         price: Joi.string(),
         title: Joi.string()
